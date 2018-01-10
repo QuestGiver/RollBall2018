c6af3ae0864281820bea0268ccd9b5f9bc797514
Paul Gibbons <s179054@students.aie.edu.au>
2018-01-10 11:59:07 -0800

    Added wandering AI

---
+++ Assets/Wander.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float distance;
+    public float radius;
+    public float speed;
+
+    public float wanderTime;
+    float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    Vector3 doWander()
+    {
+        //project a point infront
+        Vector3 circlePos = transform.position + (transform.forward * distance);
+
+
+        //genrate random direction based on unitsphere and add to projected point.
+        Vector3 wanderTarget = (Random.insideUnitSphere * radius);
+        wanderTarget += circlePos;
+
+
+        //calc direction to wander target and return normalized
+        Vector3 dir = transform.position - wanderTarget;
+        
+        return dir.normalized;
+    }
+
+    // Update is called once per frame
+    public Vector3 moveDir;
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            moveDir = doWander();
+            timer = wanderTime;
+        }
+
+        rb.AddForce(moveDir * speed);
+
+        
+    }
+}
