bb9314568984d96459f7e0b74400bb75a55ffeb7
Paul Gibbons <s179054@students.aie.edu.au>
2018-01-16 13:01:50 -0800

    Added UI

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Load : MonoBehaviour
+{
+    public void LoadIndex(int idx)
+    {
+        SceneManager.LoadScene(idx);
+    }
+	
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TimerScript : MonoBehaviour
+{
+    public Text timerLabel;
+
+    public Rigidbody player;
+
+    public float duration;
+
+    public bool isRunning = true;
+
+    void Update()
+    {
+        duration -= Time.deltaTime;
+
+        int temp = (int)duration;
+
+        int hours =     (temp/60/60);
+        int minuntes =  (temp/60);
+        int seconds =   (temp % 60);
+        int speed = (int)player.velocity.magnitude;
+
+        timerLabel.text = hours.ToString() + ":" + minuntes.ToString() + ":" + seconds.ToString() + "\n \t\t Speed:" + speed + " Mps";
+    }
+
+    public void ToggleTimer()
+    {
+        enabled = !enabled;
+    }
+
+}
